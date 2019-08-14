b7d7aa4d458218c20fb33758a2f19c8e000bdab5
Shahan Akhter <shahan3.14@gmail.com>
2019-08-14 00:38:27 -0400

    added simple platformer project code and scenes to be worked with

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+     // Set mass to 100. Set gravity scale to 3. Freeze rotation along Z-axis.
+    Rigidbody rb;
+
+    public float speed = 5.0f;
+    public float jumpSpeed = 12.0f;
+
+    public bool canJump = false;
+    public bool canMove = true;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Explains 'FixedUpdate' -> http://unity3d.com/learn/tutorials/topics/scripting/update-and-fixedupdate
+    void FixedUpdate()
+    {
+        // Explains 'GetAxis' -> https://unity3d.com/learn/tutorials/topics/scripting/getaxis
+        float xinput = Input.GetAxis("Horizontal");
+
+        if (canMove)
+            rb.velocity = new Vector2(xinput * speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            canJump = false;
+        }
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "ground")
+            canJump = true;
+    }
+// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float xinput;
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool canJump = false;
+
+    Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xinput = Input.GetAxis("Horizontal");
+
+        playerRB.velocity = new Vector2(xinput * moveSpeed, playerRB.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && canJump)
+        {
+            playerRB.AddForce(new Vector2(0,jumpForce), ForceMode.Impulse);
+            canJump = false;
+        }
+        
+    }
+
+/*
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "ground")
+        {
+            canJump = true;
+        }
+    }
+    */
+}
