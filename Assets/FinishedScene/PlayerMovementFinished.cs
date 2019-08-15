0aa94dd035823daaceaea374658fe9adab2a8c09
Shahan Akhter <shahan3.14@gmail.com>
2019-08-15 15:53:41 -0400

    commented finished script

--- Assets/FinishedScene/PlayerMovementFinished.cs
+++ Assets/FinishedScene/PlayerMovementFinished.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovementFinished : MonoBehaviour
 {
-    float xinput;
+    // In Unity 'public' also exposes variables to the inspector, which is useful for debugging and learning
+    public float xinput;
     public float moveSpeed;
     public float jumpForce;
 
@@ -21,8 +22,10 @@ public class PlayerMovementFinished : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Unity function that get's input from WASD or the arrow keys. Returns -1 or 1
         xinput = Input.GetAxis("Horizontal");
 
+        // Keep the y-velocity so we can jump
         playerRB.velocity = new Vector2(xinput * moveSpeed, playerRB.velocity.y);
 
         if(Input.GetKeyDown(KeyCode.UpArrow) && canJump)
@@ -33,6 +36,7 @@ public class PlayerMovementFinished : MonoBehaviour
         
     }
 
+    // Unity function that gives us access to anything colliding with this object
     void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "ground")
